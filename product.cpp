09603828a99eafac408828fea38a241fcc2e92bd
Anjena061 <83183023+Anjena061@users.noreply.github.com>
2021-05-16 21:14:14 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    float a=0, b=0, pro=0;
+     cout<<"Enter the two numbers whose product in to be found"<<endl;
+     cin>>a>>b;
+     pro=a*b;
+     cout<<"The product is "<<pro;
+
+}
